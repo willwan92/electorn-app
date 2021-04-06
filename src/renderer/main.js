4c86cfa06dfa6904a8e4ad82ecd95fe1f830b7de
willwan92 <wilson3cy@163.com>
2021-04-06 22:23:09 +0800

    引入moment.js

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,7 @@ import 'normalize.css/normalize.css'// A modern alternative to CSS resets
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
+import moment from 'moment'
 
 import App from './App'
 import router from './router'
@@ -17,6 +18,7 @@ if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 
 Vue.use(ElementUI, { locale })
 Vue.prototype.$ELEMENT = { size: 'small' }
+Vue.prototype.$moment = moment
 
 Vue.config.productionTip = false
 
