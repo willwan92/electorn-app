6f34f503659ced616d679c2a2b741e09a56173f7
willwan92 <wilson3cy@163.com>
2021-04-09 17:24:21 +0800

    添加专用规则页面

---
+++ src/renderer/views/specialRules/complexRule/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>特殊规则-复杂</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/renderer/views/specialRules/simpleRule/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>特殊规则-简单</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
