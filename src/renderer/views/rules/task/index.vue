0b3808d94b0885c638e52180a077320116960895
willwan92 <wilson3cy@163.com>
2021-04-15 16:48:57 +0800

    任务名称规则和任务时长规则增加表单验证

--- src/renderer/views/rules/task/index.vue
+++ src/renderer/views/rules/task/index.vue
@@ -1,30 +1,38 @@
 <template>
   <el-card header="操作任务名称规则">
-    <el-form class="page-form" :model="form" label-width="100px" size="small">
+    <el-form class="page-form" :model="form" ref="form" :inline="true" label-width="100px" size="small">
       <el-form-item label="规则说明：">
         操作任务名称不可包含设置的关键字，否则报出设置的错误信息
       </el-form-item>
+      <br>
       <el-form-item label="校验逻辑：">
         <el-select v-model="form.operator">
           <el-option value="notIn" label="不可包含"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="关键字：" class="keywords">
-        <el-row  v-for="(item, index) in form.keywords" :key="index" style="margin-bottom: 6px;">
-          <el-col :span="10">
-            <el-input v-model="form.keywords[index].keyword" placeholder="请输入关键字"></el-input>
-          </el-col>
-          <el-col class="line" :span="2">：</el-col>
-          <el-col :span="10">
-            <el-input v-model="form.keywords[index].errorMsg" placeholder="请输入报错信息"></el-input>
-          </el-col>
-          <el-col  class="button" :span="2">
-            <el-button v-if="index === 0" icon="el-icon-plus" @click="handleAddClick" circle></el-button>
-            <el-button v-else icon="el-icon-minus" @click="handleRemoveClick(index)" type="danger" circle></el-button>
-          </el-col>
-        </el-row>
-      </el-form-item>
-      <el-form-item label-width="">
+      <br>
+      <div v-for="(item, index) in form.keywords" :key="index">
+        <el-form-item
+          :label="`关键字${index}：`"
+          :prop="`keywords.${index}.keyword`"
+          :rules="{
+            required: true, message: '请输入关键字', trigger: 'blur'
+          }">
+          <el-input v-model="form.keywords[index].keyword" placeholder="请输入关键字"></el-input>
+        </el-form-item>
+        <el-form-item
+          :prop="`keywords.${index}.errorMsg`"
+          :rules="{
+            required: true, message: '请输入报错信息', trigger: 'blur'
+          }">
+          <el-input v-model="form.keywords[index].errorMsg" placeholder="请输入报错信息"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button v-if="index === 0" icon="el-icon-plus" @click="handleAddClick" circle></el-button>
+          <el-button v-else icon="el-icon-minus" @click="handleRemoveClick(index)" type="danger" circle></el-button>
+        </el-form-item>
+      </div>
+      <el-form-item label=" ">
         <el-button type="primary" @click="handleSubmit">保存</el-button>
       </el-form-item>
     </el-form>
@@ -66,15 +74,18 @@ export default {
       this.form.keywords.splice(index, 1)
     },
     async handleSubmit () {
-      db.nameRule
-        .put(this.form, 'notIn')
-        .then(() => {
-          this.dialogFormVisible = false
-          this.$message.success('保存成功')
-        })
-        .catch(err => {
-          this.$message.error(`錯誤：${err.message || err}`)
-        })
+      this.$refs.form.validate(valid => {
+        if (!valid) return
+        db.nameRule
+          .put(this.form, 'notIn')
+          .then(() => {
+            this.dialogFormVisible = false
+            this.$message.success('保存成功')
+          })
+          .catch(err => {
+            this.$message.error(`錯誤：${err.message || err}`)
+          })
+      })
     }
   }
 }
@@ -83,8 +94,8 @@ export default {
 <style lang="scss" scoped>
 .page-form {
   width: 600px;
-  .el-select, .el-input {
-    width: 500px;
+  .el-select {
+    width: 200px;
   }
   .keywords {
     .line {
@@ -92,9 +103,6 @@ export default {
     }
     .button {
       text-align: right;
-    }
-    .el-input {
-      width: 100%;
     }
   }
 }
--- src/renderer/views/rules/time/index.vue
+++ src/renderer/views/rules/time/index.vue
@@ -1,6 +1,6 @@
 <template>
   <el-card header="操作时间规则">
-    <el-form class="page-form" :model="form" label-width="100px" size="small">
+    <el-form class="page-form" :model="form" ref="form" label-width="100px" size="small">
       <el-form-item label="规则说明：">
         操作开始时间至操作结束时间要满足设置的时长规则
       </el-form-item>
@@ -9,12 +9,20 @@
           <el-option value="gt" label="要大于"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="时长：">
-        <el-input placeholder="请输入时长" v-model="form.timeLength">
+      <el-form-item
+        label="时长："
+        prop="timeLength"
+        :rules="[{
+          required: true, message: '请输入时长', trigger: 'blur'
+        }, { 
+          type: 'number', min: 1, message: '时长必须为大于等于 1 的数字'
+        }]
+      ">
+        <el-input v-model.number="form.timeLength" placeholder="请输入时长" >
           <template slot="append">分钟</template>
         </el-input>
       </el-form-item>
-      <el-form-item label-width="" >
+      <el-form-item label-width="">
         <el-button type="primary" @click="handleSubmit">保存</el-button>
       </el-form-item>
     </el-form>
@@ -44,22 +52,18 @@ export default {
       }
     },
     async handleSubmit () {
-      const timeLength = this.form.timeLength
-      if (!timeLength) {
-        return this.$message.error('请输入时长')
-      } else if (!(Number(timeLength) > 0)) {
-        return this.$message.error('时长请输入大于零的数字')
-      }
-
-      db.timeRule
-        .put(this.form, 'gt')
-        .then(() => {
-          this.dialogFormVisible = false
-          this.$message.success('保存成功')
-        })
-        .catch(err => {
-          this.$message.error(`錯誤：${err.message || err}`)
-        })
+      this.$refs.form.validate(valid => {
+        if (!valid) return
+        db.timeRule
+          .put(this.form, 'gt')
+          .then(() => {
+            this.dialogFormVisible = false
+            this.$message.success('保存成功')
+          })
+          .catch(err => {
+            this.$message.error(`錯誤：${err.message || err}`)
+          })
+      })
     }
   }
 }
