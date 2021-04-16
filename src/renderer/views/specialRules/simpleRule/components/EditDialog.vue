15c1c02f8d71c9c5682f573fd2985e4906577e08
willwan92 <wilson3cy@163.com>
2021-04-16 15:39:25 +0800

    专用简单规则增加表单验证

--- src/renderer/views/specialRules/simpleRule/components/EditDialog.vue
+++ src/renderer/views/specialRules/simpleRule/components/EditDialog.vue
@@ -1,7 +1,7 @@
 <template>
   <el-dialog custom-class="dialog" width="600px" :title="form.id ? '编辑简单规则' : '新建简单规则'" :visible.sync="dialogFormVisible">
-    <el-form :model="form" label-width="100px">
-      <el-form-item label="规则名称">
+    <el-form :model="form" :rules="rules" ref="form" label-width="auto">
+      <el-form-item label="规则名称" prop="name">
         <el-input v-model="form.name" autocomplete="off"></el-input>
       </el-form-item>
       <el-form-item label="工作地点">
@@ -14,8 +14,8 @@
           ></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="任务名称条件">
-        <el-select v-model="form.taskCondition.operator" placeholder="请选择筛选逻辑" style="margin-bottom: 10px;">
+      <el-form-item class="keyword" label="任务名称条件" prop="taskCondition.operator">
+        <el-select v-model="form.taskCondition.operator" placeholder="请选择筛选逻辑">
           <el-option
             v-for="item in operatorOptions"
             :label="item.filterLabel"
@@ -23,32 +23,39 @@
             :value="item.value"
           ></el-option>
         </el-select>
-        <div v-for="(item, index) in form.taskCondition.keywords" :key="index">
-          <el-input v-model="form.taskCondition.keywords[index]" placeholder="请输入关键字，关键字之间是或的关系"></el-input>
-          <el-button v-if="index === 0" icon="el-icon-plus" @click="handleAddClick(form.taskCondition.keywords)" circle></el-button>
-          <el-button class="btn-del" v-else icon="el-icon-minus" @click="handleRemoveClick(form.taskCondition.keywords, index)" type="danger" circle></el-button>
-        </div>
       </el-form-item>
-      <el-form-item label="校验逻辑">
+      <el-form-item
+        v-for="(item, index) in form.taskCondition.keywords"
+        :key="`cond-${index}`"
+        :class="index === form.taskCondition.keywords.length - 1 ? 'last-keyword' : 'keyword'"
+        :prop="`taskCondition.keywords.${index}`">
+        <el-input v-model="form.taskCondition.keywords[index]" placeholder="请输入关键字，关键字之间是或的关系"></el-input>
+        <el-button v-if="index === 0" class="btn-del" icon="el-icon-plus" @click="handleAddCondKeyword" circle></el-button>
+        <el-button class="btn-del" v-else icon="el-icon-minus" @click="handleRemoveCondKeyword(index)" type="danger" circle></el-button>
+      </el-form-item>
+      <el-form-item label="校验逻辑" prop="operator">
         <el-select v-model="form.operator" placeholder="请选择校验逻辑">
           <el-option label="需包含" value="in"></el-option>
           <el-option label="不可包含" value="notIn"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="校验关键字">
-        <div v-for="(item, index) in form.keywords" :key="index">
-          <el-input v-model="form.keywords[index]" placeholder="请输入关键字，关键字之间是或的关系"></el-input>
-          <el-button v-if="index === 0" icon="el-icon-plus" @click="handleAddClick(form.keywords)" circle></el-button>
-          <el-button class="btn-del" v-else icon="el-icon-minus" @click="handleRemoveClick(form.keywords, index)" type="danger" circle></el-button>
-        </div>
+      <el-form-item
+        v-for="(item, index) in form.keywords"
+        :key="index"
+        :label="index === 0 ? '校验关键字' : ''"
+        :class="index === form.keywords.length - 1 ? 'last-keyword' : 'keyword'"
+        :prop="`keywords.${index}`">
+        <el-input v-model="form.keywords[index]" placeholder="请输入关键字，关键字之间是或的关系"></el-input>
+        <el-button v-if="index === 0" icon="el-icon-plus" @click="handleAddCheckKeyword" circle></el-button>
+        <el-button class="btn-del" v-else icon="el-icon-minus" @click="handleRemoveCheckKeyword(index)" type="danger" circle></el-button>
       </el-form-item>
-      <el-form-item label="报错信息">
+      <el-form-item label="报错信息" prop="errorMsg">
         <el-input v-model="form.errorMsg"></el-input>
       </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
       <el-button @click="dialogFormVisible = false">取 消</el-button>
-      <el-button type="primary" @click="handleSubmit">确 定</el-button>
+      <el-button type="primary" :disabled="isSubmiting" :loading="isSubmiting" @click="handleSubmit">确 定</el-button>
     </div>
   </el-dialog>
 </template>
@@ -62,6 +69,7 @@ export default {
     return {
       formLabelWidth: '120px',
       dialogFormVisible: false,
+      isSubmiting: false,
       operatorOptions: Object.freeze(operatorOptions),
       workplaceList: Object.freeze(workplaceList),
       form: {
@@ -74,6 +82,32 @@ export default {
         operator: '',
         keywords: [''],
         errorMsg: ''
+      },
+      rules: {
+        name: [{
+          required: true, message: '请输入规则名称', trigger: 'blur'
+        }],
+        taskCondition: {
+          operator: [{
+            required: true, message: '请选择校验逻辑', trigger: 'change'
+          }],
+          keywords: [
+            [{
+              required: true, message: '请输入关键字', trigger: 'blur'
+            }]
+          ],
+        },
+        operator: [{
+          required: true, message: '请选择校验逻辑', trigger: 'change'
+        }],
+        keywords: [
+          [{
+            required: true, message: '请输入关键字', trigger: 'blur'
+          }]
+        ],
+        errorMsg: [{
+          required: true, message: '请输入报错信息', trigger: 'blur'
+        }],
       }
     }
   },
@@ -96,39 +130,65 @@ export default {
         this.form = await db.specialSimpleRule.get(row.id)
       }
     },
-    handleAddClick (target) {
-      target.push('')
+    handleAddCondKeyword () {
+      this.form.taskCondition.keywords.push('')
+      this.rules.taskCondition.keywords.push([{
+        required: true, message: '请输入关键字', trigger: 'blur'
+      }])
     },
-    handleRemoveClick (target, index) {
-      target.splice(index, 1)
+    handleRemoveCondKeyword (index) {
+      this.form.taskCondition.keywords.splice(index, 1)
+      this.rules.taskCondition.keywords.splice(index, 1)
+    },
+    handleAddCheckKeyword () {
+      this.form.keywords.push('')
+      this.rules.keywords.push([{
+        required: true, message: '请输入关键字', trigger: 'blur'
+      }])
+    },
+    handleRemoveCheckKeyword (index) {
+      this.form.keywords.splice(index, 1)
+      this.rules.keywords.splice(index, 1)
     },
     handleSubmit () {
-      if (!this.form.id) {
-        db.specialSimpleRule
-          .add({
-            ...this.form
-          })
-          .then(() => {
-            this.dialogFormVisible = false
-            this.$message.success('添加成功')
-            this.$emit('ok')
-          })
-          .catch(err => {
-            this.$message.error(`錯誤：${err.message || err}`)
-          })
-      } else {
-        const rule = { ...this.form }
-        db.specialSimpleRule
-          .update(this.form.id, rule)
-          .then(() => {
-            this.dialogFormVisible = false
-            this.$message.success('修改成功')
-            this.$emit('ok')
-          })
-          .catch(err => {
-            this.$message.error(`錯誤：${err.message || err}`)
-          })
-      }
+      // 防止重复提交
+      if (this.isSubmiting) return
+      this.$refs.form.validate(valid => {
+        if (!valid) return
+        this.isSubmiting = true
+        if (!this.form.id) {
+          db.specialSimpleRule
+            .add({
+              ...this.form
+            })
+            .then(() => {
+              this.dialogFormVisible = false
+              this.$message.success('添加成功')
+              this.$emit('ok')
+            })
+            .catch(err => {
+              this.$message.error(`錯誤：${err.message || err}`)
+            })
+            .finally(() => {
+              this.isSubmiting = false
+            })
+        } else {
+          const rule = { ...this.form }
+          db.specialSimpleRule
+            .update(this.form.id, rule)
+            .then(() => {
+              this.dialogFormVisible = false
+              this.$message.success('修改成功')
+              this.$emit('ok')
+            })
+            .catch(err => {
+              this.$message.error(`錯誤：${err.message || err}`)
+            })
+            .finally(() => {
+              this.isSubmiting = false
+            })
+        }
+      })
     }
   }
 }
@@ -138,10 +198,16 @@ export default {
 .dialog {
   width: 400px;
   .el-input, .el-select {
-    width: 300px;
+    width: 320px;
   }
   .btn-del {
-    margin-top: 10px;
+    margin-top: 14px;
+  }
+  .keyword {
+    margin-bottom: 0px;
+  }
+  .last-keyword {
+    margin-bottom: 20px;
   }
 }
 </style>
