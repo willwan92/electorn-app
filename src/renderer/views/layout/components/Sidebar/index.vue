b981b8f74576478783525000cf01ced4ac6dbce9
willwan92 <wilson3cy@163.com>
2021-04-22 18:29:02 +0800

    添加logo并修改侧边栏颜色

--- src/renderer/views/layout/components/Sidebar/index.vue
+++ src/renderer/views/layout/components/Sidebar/index.vue
@@ -1,11 +1,14 @@
 <template>
   <scroll-bar>
+    <div class="logo-wrapper">
+      <img class="logo" :src="logoImg" alt="logo">
+    </div>
     <el-menu
       mode="vertical"
       :show-timeout="200"
       :default-active="$route.path"
       :collapse="isCollapse"
-      background-color="#304156"
+      background-color="#606266"
       text-color="#bfcbd9"
       active-text-color="#409EFF"
     >
@@ -18,9 +21,15 @@
 import { mapGetters } from 'vuex'
 import SidebarItem from './SidebarItem'
 import ScrollBar from '@/components/ScrollBar'
+import logoImg from '@/assets/logo.png'
 
 export default {
   components: { SidebarItem, ScrollBar },
+  data () {
+    return {
+      logoImg: logoImg
+    }
+  },
   computed: {
     ...mapGetters([
       'sidebar',
@@ -32,3 +41,16 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+.logo-wrapper {
+  width: 100%;
+  height: 50px;
+  background-color: #EBEEF5;
+  text-align: center;
+  padding-top: 11px;
+  .logo {
+    width: 160px;
+  }
+}
+</style>
